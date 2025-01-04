8b08e5f1c38917920fde55d3352a77787721895f
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-01-04 16:59:38 +0200

    finished Port

--- Testing/Lab1/src/main.c
+++ Testing/Lab1/src/main.c
@@ -11,13 +11,13 @@
 int main(void)
 {
     MPORT_vInit();
-   /* MDIO_enuSetPinConfigration(MDIO_enuPORTA,MDIO_enuPIN0,MDIO_enuPIN_OUTPUT);
-    MDIO_enuSetPinConfigration(MDIO_enuPORTA,MDIO_enuPIN1,MDIO_enuPIN_OUTPUT);
-    MDIO_enuSetPinConfigration(MDIO_enuPORTA,MDIO_enuPIN2,MDIO_enuPIN_OUTPUT);
-    MDIO_enuSetPinConfigration(MDIO_enuPORTA,MDIO_enuPIN3,MDIO_enuPIN_INPUT_PULLUP);
-    MDIO_enuSetPinConfigration(MDIO_enuPORTA,MDIO_enuPIN4,MDIO_enuPIN_INPUT_PULLUP);
-    MDIO_enuSetPinConfigration(MDIO_enuPORTA,MDIO_enuPIN5,MDIO_enuPIN_INPUT_PULLUP);
-    */
+   MPORT_enuSetPinDirection(PIN_A0,MPORT_OUTPUT);
+   MPORT_enuSetPinDirection(PIN_A1,MPORT_OUTPUT);
+   MPORT_enuSetPinDirection(PIN_A2,MPORT_OUTPUT);
+   MPORT_enuSetPinDirection(PIN_A3,MPORT_INPUT);
+   MPORT_enuSetPinDirection(PIN_A4,MPORT_INPUT);
+   MPORT_enuSetPinDirection(PIN_A5,MPORT_INPUT);
+   
     u8 Local_u8PinValue = 0 ;
     while(1)
     {
